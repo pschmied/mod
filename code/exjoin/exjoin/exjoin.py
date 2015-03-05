b370560538bf02002bef5901ada5cb1fdd0e8071
Peter Schmiedeskamp <peter@thoughtspot.net>
2015-03-05 06:28:33 -0800

    Exhaustive join in Python / Pandas

---
+++ code/exjoin/exjoin/exjoin.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import gc, os, argparse
+from itertools import product, combinations
+from glob import glob
+from multiprocessing import Pool
+
+
+
+def cjoin(p0, p1):
+    d0 = pd.read_csv(p0, low_memory=False)
+    d1 = pd.read_csv(p1, low_memory=False)
+    comb_col = product(d0.columns, d1.columns)
+    for c in comb_col:
+        l = len(pd.merge(d0, d1, left_on=c[0],
+                         right_on=c[1], sort=False))
+        print "{}\t{}\t{}\t{}\t{}".format(p0, c[0], p1, c[1], l)
+        gc.collect()
+            
+def tjoin(x):
+    cjoin(x[0], x[1])
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Exhaustively join set of csv files")
+    parser.add_argument("csvpath", help="path to csv files to join")
+    parser.add_argument("-w", "--workers", help="number of worker processes to spawn", default=2)
+    args = parser.parse_args()
+    p = Pool(args.workers)
+    filelist = glob(os.path.join(args.csvpath, "*.csv"))
+    comb_file = combinations(filelist, 2)
+    p.map(tjoin, comb_file)
+
+if __name__ == '__main__':
+    main()
---
+++ code/exjoin/tests/exjoin_tests.py
@@ -0,0 +1,13 @@
+from nose.tools import *
+import exjoin
+
+def setup():
+    print "setup!"
+
+def teardown():
+    print "tear down!"
+
+def test_basic():
+    print "I ran!"
+
+
