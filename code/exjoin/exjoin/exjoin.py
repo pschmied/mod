593e498858ef8983da7c0462f67874a5dfed48a2
Peter Schmiedeskamp <peter@thoughtspot.net>
2015-03-05 07:39:29 -0800

    Add locking when writing to std.out

--- code/exjoin/exjoin/exjoin.py
+++ code/exjoin/exjoin/exjoin.py
@@ -1,20 +1,27 @@
 import pandas as pd
-import gc, os, argparse
+import gc, os, argparse, sys
 from itertools import product, combinations
 from glob import glob
-from multiprocessing import Pool
+from multiprocessing import Pool, Lock
 
+lock = Lock()
 
+def output(res):
+    with lock:
+        sys.stdout.write(res)
 
 def cjoin(p0, p1):
     d0 = pd.read_csv(p0, low_memory=False)
     d1 = pd.read_csv(p1, low_memory=False)
     comb_col = product(d0.columns, d1.columns)
+    res = []
     for c in comb_col:
         l = len(pd.merge(d0, d1, left_on=c[0],
                          right_on=c[1], sort=False))
-        print "{}\t{}\t{}\t{}\t{}".format(p0, c[0], p1, c[1], l)
+        res.append("{}\t{}\t{}\t{}\t{}\n".format(p0, c[0], p1, c[1], l))
         gc.collect()
+    for x in res:
+        output(x)
             
 def tjoin(x):
     cjoin(x[0], x[1])
