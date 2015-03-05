5494843836210ed24c5864aed17ead2db9aea24d
Peter Schmiedeskamp <peter@thoughtspot.net>
2015-03-05 09:21:37 -0800

    use set intersection rather than inner join

--- code/exjoin/exjoin/exjoin.py
+++ code/exjoin/exjoin/exjoin.py
@@ -10,32 +10,31 @@ def output(res):
     with lock:
         sys.stdout.write(res)
 
-def cjoin(p0, p1):
-    d0 = pd.read_csv(p0, low_memory=False)
-    d1 = pd.read_csv(p1, low_memory=False)
-    comb_col = product(d0.columns, d1.columns)
+def cjoin(pathstuple):
+    d0 = pd.read_csv(pathstuple[0], low_memory=False)
+    d1 = pd.read_csv(pathstuple[1], low_memory=False)
+    s0 = d0.apply(set, 1)
+    s1 = d1.apply(set, 1)
+    cols = product(s0, s1)
+    names = product(d0.columns, d1.columns)
     res = []
-    for c in comb_col:
-        l = len(pd.merge(d0, d1, left_on=c[0],
-                         right_on=c[1], sort=False))
-        res.append("{}\t{}\t{}\t{}\t{}\n".format(p0, c[0], p1, c[1], l))
+    for c,n in zip(cols, names):
+        l = len(c[0].intersection(c[1]))
+        res.append("{}\t{}\t{}\t{}\t{}\n".format(pathstuple[0], n[0], pathstuple[1], n[1], l))
         gc.collect()
     for x in res:
         output(x)
             
-def tjoin(x):
-    cjoin(x[0], x[1])
-
-
 def main():
     parser = argparse.ArgumentParser(description="Exhaustively join set of csv files")
     parser.add_argument("csvpath", help="path to csv files to join")
     parser.add_argument("-w", "--workers", help="number of worker processes to spawn", default=2)
     args = parser.parse_args()
+
     p = Pool(args.workers)
     filelist = glob(os.path.join(args.csvpath, "*.csv"))
-    comb_file = combinations(filelist, 2)
-    p.map(tjoin, comb_file)
+    pathstuple = combinations(filelist, 2)
+    p.map(cjoin, pathstuple)
 
 if __name__ == '__main__':
     main()
